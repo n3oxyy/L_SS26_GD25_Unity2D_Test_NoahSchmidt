39e02a77f6aa58ff3b0b91b1d0b198baad868a17
n3oxyy <noah.schmidt112005@gmail.com>
2026-03-11 11:08:21 +0100

    push
    2 buttons geaddet und script zusammengestellt aus tutorials

---
+++ Test/Assets/Scripts/Logic.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class TestManager : MonoBehaviour
+{
+    
+    public TextMeshProUGUI valueText;
+    public GameObject screen1;
+    public GameObject screen2;
+    public AudioSource audioSource;
+    public AudioClip correctSound;
+    
+
+    private int value = 0;
+    private int dynamicAddAmount = 1;
+    
+
+   
+   
+
+   //knopf für nachricht an console: dunktioniert
+    public void Button1_MessageToConsole()
+    {
+        Debug.Log("Test_1_Button_1_pressed");
+    }
+
+  // der geht irgendwie nich, nochmal ändern mit anderem tutorial
+    public void Button2_PlaySound()
+    {
+        audioSource.PlayOneShot(correctSound);
+    }
+
+    //der machtt
+    public void Button3_OpenScreen1()
+    {
+        screen1.SetActive(true);
+    }
+
+    
+    public void Button4_OpenScreen2()
+    {
+        screen2.SetActive(true);
+    }
+
+    
+    public void BackButton_CloseScreen1()
+    {
+        screen1.SetActive(false);
+    }
+
+    
+    public void BackButton_CloseScreen2()
+    {
+        screen2.SetActive(false);
+    }
+
+   
+}
